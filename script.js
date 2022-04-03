9fda628a27f4072910d3a0e340145dda3757a47a
Brandon Moye <85130587+Brandon-Moye@users.noreply.github.com>
2022-04-03 16:18:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,178 +1,66 @@
 "use strict";
 
-let clickCount = 0; //used to check for ties later
+let turn = 0; //used to check for ties later
 let piece = ""; //will either be an X or O
-/**
- * try adding a this key word to help get the function to be refracted
- */
-document.querySelector(".r1c1").addEventListener(
-  "click",
-  function a1() {
-    if (piece === "O") {
-      piece = "X";
-    } else {
-      piece = "O";
-    }
-    clickCount++;
-    document.querySelector(".r1c1").textContent = piece;
 
-    variables();
-  },
-  { once: true } //experimenting now put this back in play once I have this figured out
-);
+function move(event) {
+  // if this square was already filled in
+  if (event.target.textContent === "O" || event.target.textContent === "X") {
+    return;
+  }
 
-document.querySelector(".r1c2").addEventListener(
-  "click",
-  function a1() {
-    if (piece === "O") {
-      piece = "X";
-    } else {
-      piece = "O";
-    }
-    clickCount++;
+  // which piece is playing
+  if (piece === "O") {
+    piece = "X";
+  } else {
+    piece = "O";
+  }
 
-    document.querySelector(".r1c2").textContent = piece;
+  // fill in the square
+  event.target.textContent = piece;
 
-    variables();
-  },
-  { once: true } //experimenting now put this back in play once I have this figured out
-);
+  // next turn
+  turn++;
 
-document.querySelector(".r1c3").addEventListener(
-  "click",
-  function a1() {
-    if (piece === "O") {
-      piece = "X";
-    } else {
-      piece = "O";
-    }
-    clickCount++;
+  checkWinner();
+  checkTie();
+}
 
-    document.querySelector(".r1c3").textContent = piece;
+document
+  .querySelector("#container")
+  .addEventListener("click", function (event) {
+    // event.target === this
+    move(event);
+  });
 
-    variables();
-  },
-  { once: true } //experimenting now put this back in play once I have this figured out
-);
+//the reset button not necessarily letting us repeat functions with the once true property
+//decided to reload the page instead so this prevents plays from clicking on a cell more than one and change the piece
+document.querySelector(".reset").addEventListener("click", function () {
+  window.location.reload();
+});
 
-document.querySelector(".r2c1").addEventListener(
-  "click",
-  function a1() {
-    if (piece === "O") {
-      piece = "X";
-    } else {
-      piece = "O";
-    }
-    clickCount++;
+function checkTie() {
+  if (turn === 9) {
+    // console.log(`${piece} You're the winner!`); //just something in the beginning phases of code
+    //turning the console.log into a query selector
+    //
+    /*
+    the final message also have a hidden class that is removed once the click
+    and add a hidden class to the container elements to removed the grid
+    */
+    document.getElementById("finalMessage").classList.remove("hidden");
+    document.getElementById("container").classList.add("hidden");
+    document.querySelector(
+      ".addMessage"
+    ).textContent = `A TIE! Please Try Again!`;
+    //in order to not allow players to continue the game we will remove the tic-tac-toe grid and a message
+    //announcing the winner will appear!
+    //adds the reset button once someone has won
+    document.getElementById("reset").classList.remove("hidden");
+  }
+}
 
-    document.querySelector(".r2c1").textContent = piece;
-
-    variables();
-  },
-  { once: true } //experimenting now put this back in play once I have this figured out
-);
-
-document.querySelector(".r2c2").addEventListener(
-  "click",
-  function a1() {
-    if (piece === "O") {
-      piece = "X";
-    } else {
-      piece = "O";
-    }
-    clickCount++;
-
-    document.querySelector(".r2c2").textContent = piece;
-
-    variables();
-  },
-  { once: true } //experimenting now put this back in play once I have this figured out
-);
-
-document.querySelector(".r2c3").addEventListener(
-  "click",
-  function a1() {
-    if (piece === "O") {
-      piece = "X";
-    } else {
-      piece = "O";
-    }
-    clickCount++;
-
-    document.querySelector(".r2c3").textContent = piece;
-
-    variables();
-  },
-  { once: true } //experimenting now put this back in play once I have this figured out
-);
-
-document.querySelector(".r3c1").addEventListener(
-  "click",
-  function a1() {
-    if (piece === "O") {
-      piece = "X";
-    } else {
-      piece = "O";
-    }
-    clickCount++;
-
-    document.querySelector(".r3c1").textContent = piece;
-
-    variables();
-  },
-  { once: true } //experimenting now put this back in play once I have this figured out
-);
-
-document.querySelector(".r3c2").addEventListener(
-  "click",
-  function a1() {
-    if (piece === "O") {
-      piece = "X";
-    } else {
-      piece = "O";
-    }
-    clickCount++;
-
-    document.querySelector(".r3c2").textContent = piece;
-
-    variables();
-  },
-  { once: true } //experimenting now put this back in play once I have this figured out
-);
-
-document.querySelector(".r3c3").addEventListener(
-  "click",
-  function a1() {
-    if (piece === "O") {
-      piece = "X";
-    } else {
-      piece = "O";
-    }
-    clickCount++;
-
-    document.querySelector(".r3c3").textContent = piece;
-
-    variables();
-  },
-  { once: true } //experimenting now put this back in play once I have this figured out
-);
-
-//experimenting with trying to shorten the code
-// function a1(cell) {
-//   if (piece === "O") {
-//     piece = "X";
-//   } else {
-//     piece = "O";
-//   }
-//   clickCount++;
-//   document.querySelector(cell).textContent = piece;
-
-//   variables();
-// }
-
-//this function checks the correct patterns
-function variables() {
-  //made variable names to make the if statement more legible
+function checkWinner() {
   let a1 = document.querySelector(".r1c1").textContent;
   let a2 = document.querySelector(".r1c2").textContent;
   let a3 = document.querySelector(".r1c3").textContent;
@@ -183,22 +71,20 @@ function variables() {
   let a8 = document.querySelector(".r3c2").textContent;
   let a9 = document.querySelector(".r3c3").textContent;
 
-  //using if statements to see if cells = i.e. a winning pattern
   if (
-    (a1 === a2 && a1 === a3) ||
-    (a1 === a4 && a1 === a7) ||
-    (a1 === a5 && a1 === a9) ||
-    (a2 === a5 && a2 === a8) ||
-    (a3 === a6 && a3 === a9) ||
-    (a3 === a5 && a3 === a7) ||
-    (a4 === a5 && a4 === a6) ||
-    (a7 === a8 && a7 === a9)
+    (a1 !== "" && a1 === a2 && a1 === a3) ||
+    (a1 !== "" && a1 === a4 && a1 === a7) ||
+    (a1 !== "" && a1 === a5 && a1 === a9) ||
+    (a2 !== "" && a2 === a5 && a2 === a8) ||
+    (a3 !== "" && a3 === a6 && a3 === a9) ||
+    (a3 !== "" && a3 === a5 && a3 === a7) ||
+    (a4 !== "" && a4 === a5 && a4 === a6) ||
+    (a7 !== "" && a7 === a8 && a7 === a9)
   ) {
     // console.log(`${piece} You're the winner!`);
     //
     //need to focus on adding an element that ends the game and doesn't allow the player to continue to click
     //turning the console.log into a query selector
-
     //if the statement is met then whoever won will be displayed using a template literal
     document.getElementById("finalMessage").classList.remove("hidden");
 
@@ -213,44 +99,6 @@ function variables() {
     */
     document.getElementById("reset").classList.remove("hidden");
   } else {
-    console.log("Keep going!"); //just something to help me when writing the code
+    console.log("Keep going!");
   }
-
-  //the reset button not necessarily letting us repeat functions with the once true property
-  //decided to reload the page instead so this prevents plays from clicking on a cell more than one and change the piece
-  document.querySelector(".reset").addEventListener("click", function () {
-    window.location.reload();
-  });
-
-  /*if a there is a win, the code above will fire
-  this is an if statement in the events of a tie
-  */
-  if (clickCount === 9) {
-    // console.log(`${piece} You're the winner!`); //just something in the beginning phases of code
-    //turning the console.log into a query selector
-
-    //
-
-    /*
-    the final message also have a hidden class that is removed once the click
-    and add a hidden class to the container elements to removed the grid
-    */
-    document.getElementById("finalMessage").classList.remove("hidden");
-    document.getElementById("container").classList.add("hidden");
-    document.querySelector(
-      ".addMessage"
-    ).textContent = `A TIE! Please Try Again!`;
-    //in order to not allow players to continue the game we will remove the tic-tac-toe grid and a message
-    //announcing the winner will appear!
-
-    //adds the reset button once someone has won
-    document.getElementById("reset").classList.remove("hidden");
-  } else {
-    // console.log("Keep going!"); //something for when I was developing
-  }
-
-  //the reset button not necessarily letting us repeat functions with the once true property
-  document.querySelector(".reset").addEventListener("click", function () {
-    window.location.reload();
-  });
 }
